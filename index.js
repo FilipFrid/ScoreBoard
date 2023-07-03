3dca918cbb27ced8877009d84bc13187714e161b
FilipFrid <138389147+FilipFrid@users.noreply.github.com>
2023-07-03 22:00:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -128,19 +128,3 @@ function winnerIs (){
 }}
 
 
-
-var myCanvas = document.createElement('canvas');
-document.body.appendChild(myCanvas);
-
-var myConfetti = confetti.create(myCanvas, {
-  resize: true,
-  useWorker: true
-});
-myConfetti({
-  particleCount: 100,
-  spread: 160
-  // any other options from the global
-  // confetti function
-});
-
-
